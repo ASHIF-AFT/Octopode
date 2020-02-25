a953b5355d64a846035f09bb9d2c6b238e95f698
PvtSec <akssofts@gmail.com>
2020-02-25 21:45:32 +0530

    Dashboard edits

--- app/src/main/java/octopode/gecw/Dashboard.java
+++ app/src/main/java/octopode/gecw/Dashboard.java
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import pl.pawelkleczkowski.customgauge.CustomGauge;
+
 public class Dashboard extends AppCompatActivity
 {
+    CustomGauge temp,humi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        temp = findViewById(R.id.temperature);
+        humi = findViewById(R.id.humidity);
+
+        temp.setPointSize(170);
     }
 }
