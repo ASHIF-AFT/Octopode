41c3ad286b0dd9e530235eec2f7e1ee733124b24
PvtSec <akssofts@gmail.com>
2020-02-25 22:04:12 +0530

    Dashboard Edits

--- app/src/main/java/octopode/gecw/Dashboard.java
+++ app/src/main/java/octopode/gecw/Dashboard.java
@@ -13,11 +13,13 @@ public class Dashboard extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        HideSyS_UI.hideui(getWindow().getDecorView());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         temp = findViewById(R.id.temperature);
         humi = findViewById(R.id.humidity);
 
         temp.setPointSize(170);
+        humi.setPointSize(100);
     }
 }
